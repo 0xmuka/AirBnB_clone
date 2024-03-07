3cd90292e2b84c7afc4110066deeb2ba60588866
0xmuka <hemasallam03@gmail.com>
2024-03-07 18:19:42 +0200

    doc

--- models/base_model.py
+++ models/base_model.py
@@ -3,5 +3,8 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
+    """Base Model for project"""
+    
     def __init__(self):
+        """Intialize a Base Model"""
         self.id = str(uuid.uuid4)
