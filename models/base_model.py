f8e190c82e3467801e1223397ea16c2d8e6a3699
0xmuka <hemasallam03@gmail.com>
2024-03-08 16:41:04 +0200

    task4

--- models/base_model.py
+++ models/base_model.py
@@ -5,9 +5,23 @@ from datetime import datetime
 class BaseModel:
     """Base Model for project"""
     
-    def __init__(self):
-        """Intialize a Base Model"""
-        self.id = str(uuid.uuid4)
+    def __init__(self, *args, **kwargs):
+        """Initialize a Base Model"""
+        if kwargs:
+            if '__class__' in kwargs:
+                kwargs.pop('__class__')  # Remove __class__ key if present
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4)
+            
+            
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
