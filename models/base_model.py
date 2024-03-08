4ac32cd8685278f5fd896ab9094ea64aab7a9a66
0xmuka <hemasallam03@gmail.com>
2024-03-08 10:35:43 +0200

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -8,3 +8,18 @@ class BaseModel:
     def __init__(self):
         """Intialize a Base Model"""
         self.id = str(uuid.uuid4)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        
+    def __str__(self) -> str:
+        """ 
+             should print: [<class name>] (<self.id>) <self.__dict__>
+        """
+        return ("[{}] ({}) {}").format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        self.updated_at = datetime.now()
+        
+    def to_dict(self):
+            pass
