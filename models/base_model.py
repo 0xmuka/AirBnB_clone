e701ff0d9b4672b9b4faa35019f23b1760ac4e64
0xmuka <hemasallam03@gmail.com>
2024-03-08 11:05:20 +0200

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,15 @@ class BaseModel:
         return ("[{}] ({}) {}").format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
+        """bla bla bla"""
         self.updated_at = datetime.now()
         
     def to_dict(self):
-            pass
+        """bla bla bla"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+    
+    
