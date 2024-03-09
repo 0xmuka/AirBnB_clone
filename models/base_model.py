c029ca4964beca2f06d06fddf49a610dba382e52
0xmuka <hemasallam03@gmail.com>
2024-03-09 15:23:10 +0200

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -14,9 +14,11 @@ class BaseModel:
                 if key != '__class__':
                     setattr(self, key, value)
             if 'created_at' in kwargs:
-                self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                self.created_at = datetime.strptime(kwargs[
+                    'created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             if 'updated_at' in kwargs:
-                self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                self.updated_at = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -24,7 +26,8 @@ class BaseModel:
 
     def __str__(self) -> str:
         """Should print: [<class name>] (<self.id>) <self.__dict__>"""
-        return ("[{}] ({}) {}").format(self.__class__.__name__, self.id, self.__dict__)
+        return ("[{}] ({}) {}").format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Update updated_at attribute to current datetime"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,31 +1,24 @@
 class FileStorage:
     """
-     class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances
-    """    
-    __file_path = "file.json"
+        Class FileStorage serializes instances
+        to a JSON file and deserializes JSON file to instances
+    """
+    __FILE_PATH = "file.json"
     __objects = {}
 
     def all(self):
-        """ 
-        returns the dictionary __objects
-        """
-        return FileStorage.__objects
-    
+        """Return the dictionary __objects."""
+        return self.__class__.__objects
+
     def new(self, obj):
-        """ 
-        sets in __objects the obj with key <obj class name>.id
-        """
-        key = f"{obj.__class__.__name__}.{obj.id}" 
-        self.__objects[key] = obj 
-        
-    
+        """Sets obj in __objects with key <obj class name>.id."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__class__.__objects[key] = obj
+
     def save(self):
-        """ 
-        bla bla bla
-        """
+        """Save serialized objects to a JSON file."""
         pass
+
     def reload(self):
-        """ 
-        bla bla bla
-        """
+        """Reload objects from a JSON file."""
         pass
