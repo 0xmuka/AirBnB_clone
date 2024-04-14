4bb521f509e2717193eb8a1b92be8a8571bf4201
Ibrahim Sallam <106562474+0xmuka@users.noreply.github.com>
2024-04-14 19:50:55 +0200

    Update console.py

--- console.py
+++ console.py
@@ -10,14 +10,14 @@ class HBNBCommand (cmd.Cmd):
     prompt = "(hbnb)"
 
     def do_quit(self, line):
-        """To quit the program with hema"""
+        """To quit the program with hema this is test repo"""
 
         exit()
 
     def do_quit(self, line):
         """to quit from program"""
 
-        print("Good Bye")
+        print("Good Bye Bye Bye Bye Bye Bye Bye Bye Bye Bye Bye")
         print()
 
     def do_EOF(self, line):
