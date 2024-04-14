01a084287807406334476080a7322fcf5d3a2f0c
Ibrahim Sallam <106562474+0xmuka@users.noreply.github.com>
2024-04-14 18:47:47 +0200

    pycodestyle

--- console.py
+++ console.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
-import cmd 
-class HBNBCommand(cmd.Cmd):
-    """ 
+""" Console Module """
+
+import cmd
+
+
+class HBNBCommand (cmd.Cmd):
+    """
     HBNBCommand
     """
     prompt = "(hbnb)"
-    def do_quit (self , line) :
-        """
-        To quit the program with hema 
-        
-        """
-        return True 
 
-    def do_EOF (self , line) :
+    def do_quit(self, line):
+        """
+        To quit the program with hema
+
+        """
+        return True
+
+    def do_EOF(self, line):
         """
         End of file
-         
+
         """
-        return True  
-    
+        return True
+
     def do_help(self, arg: str) -> bool | None:
 
         """
         to do help
-        
+
         """
         return super().do_help(arg)
 
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-    
