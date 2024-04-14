c7c4528ce53f643cec43436a0f20adbb85e28b1e
Ibrahim Sallam <106562474+0xmuka@users.noreply.github.com>
2024-04-14 19:58:58 +0200

    Update console.py

--- console.py
+++ console.py
@@ -10,20 +10,17 @@ class HBNBCommand (cmd.Cmd):
     prompt = "(hbnb)"
 
     def do_quit(self, line):
-        """To quit the program with hema"""
+        """Quit command to exit the program
+        
+        """
 
         exit()
-
-    def do_quit(self, line):
-        """to quit from program"""
-
-        print("Good Bye")
-        print()
 
     def do_EOF(self, line):
         """End of file"""
 
-        return True
+        exit()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
