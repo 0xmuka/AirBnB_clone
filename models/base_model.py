1af154f31312c265b571ee5a298375c6de39a42a
0xmuka <hemasallam03@gmail.com>
2024-04-15 13:05:46 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -3,27 +3,19 @@ from datetime import datetime as dt
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
-    def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = dt.now()
-            self.updated_at = dt.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = dt.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = dt.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+    """
+    aykalam
+    """
+    def __init__(self) -> None:
+        """
+        initialization
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = dt.now()
+        self.updated_at = dt.now()
 
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+    def __str__(self) -> str:
+        return f"{[__class__.__name__]} {(self.id)} {self.__dict__}"
 
     def save(self):
         self.updated_at = dt.now()
