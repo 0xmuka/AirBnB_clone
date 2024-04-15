c1e990889876a2e10ecf2cdb0e1670efbb455ff3
0xmuka <hemasallam03@gmail.com>
2024-04-15 13:22:37 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -2,31 +2,34 @@ import uuid
 from datetime import datetime as dt
 
 
+
 class BaseModel:
-    """
-    aykalam
-    """
-    def __init__(self) -> None:
-        """
-        initialization
-        """
-        self.id = str(uuid.uuid4())
-        self.created_at = dt.now()
-        self.updated_at = dt.now()
+
+    def __init__(self, *args, **kwargs) -> None:
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            if 'created_at' in key:
+                self.created_at.isoformat()
+            if 'updated_at' in key:
+                self.updated_at.isoformat()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = dt.now()
+            self.updated_at = dt.now()
 
     def __str__(self) -> str:
-        return f"{[__class__.__name__]} {(self.id)} {self.__dict__}"
+        return '[{}] ({}) {}'.format(self.__class__.__name__,self.id,self.__dict__)
 
     def save(self):
+        from models import storage
         self.updated_at = dt.now()
+        storage.save()
 
     def to_dict(self):
-        obj = self.__dict__.copy()
-        obj['__class__'] = self.__class__.__name__
-        obj['created_at'] = self.created_at.isoformat()
-        obj['updated_at'] = self.updated_at.isoformat()
-        return obj
-
-
-x = BaseModel()
-print(x.to_dict())
+        object = self.__dict__.copy()
+        object['__class__'] = self.__class__.__name__
+        object['created_at'] = self.created_at.isoformat()
+        object['updated_at'] = self.updated_at.isoformat()
+        return object
