17f392a7726535857250b82cf3d730ccf9c073a9
0xmuka <hemasallam03@gmail.com>
2024-04-16 18:58:58 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,10 @@
-#!/usr/bin/python3
-"""
-BaseModel
-"""
-
-
 import uuid
 from datetime import datetime as dt
-from models import storage
 
 
 class BaseModel:
-    """all function in basemodel class"""
 
-    def __init__(self, *args, **kwargs):
-        """id / created_at / updated_at"""
+    def __init__(self, *args, **kwargs) -> None:
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -26,26 +17,17 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = dt.now()
             self.updated_at = dt.now()
-            storage.new(self)
 
-    def __str__(self):
-        """ {[<class name>] (<self.id>) <self.__dict__>}"""
-        return ("[{}] ({}) {}").format(
-          self.__class__.__name__, self.id, self.__dict__)
+    def __str__(self) -> str:
+        return '[{}] ({}) {}'.format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-        update
-        """
         self.updated_at = dt.now()
-        storage.save()
 
     def to_dict(self):
-        """
-        copy a dictionary and return new values
-        """
-        my_ob_dict = self.__dict__.copy()
-        my_ob_dict["__class__"] = self.__class__.__name__
-        my_ob_dict["created_at"] = my_ob_dict["created_at"].isoformat()
-        my_ob_dict["updated_at"] = my_ob_dict["updated_at"].isoformat()
-        return my_ob_dict
+        object = self.__dict__.copy()
+        object['__class__'] = self.__class__.__name__
+        object['created_at'] = self.created_at.isoformat()
+        object['updated_at'] = self.updated_at.isoformat()
+        return object
