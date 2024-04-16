c8e34e41e28b6ddc82f4c49f8162c8a7f1402b77
0xmuka <hemasallam03@gmail.com>
2024-04-16 18:52:04 +0200

    file storage update function save() and reload()

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = dt.now()
             self.updated_at = dt.now()
-            storage.new(self)
+            # storage.new(self)
 
     def __str__(self) -> str:
         """ convert to string"""
@@ -30,7 +30,7 @@ class BaseModel:
     def save(self):
         """save and update"""
         self.updated_at = dt.now()
-        storage.save()
+        # storage.save()
 
     def to_dict(self):
         """ to_dict fuction to convert format of dictionary"""
