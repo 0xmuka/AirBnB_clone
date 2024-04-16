43ea1b0e85a8c87f15e659621d99eb7360dabe43
0xmuka <hemasallam03@gmail.com>
2024-04-16 19:13:37 +0200

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,8 +19,9 @@ class FileStorage:
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
+        
         """convert dictionary to json file string"""
-        with open(self.__file_path, 'w', encoding="utf8") as f:
+        with open(FileStorage.__file_path, 'w', encoding="utf8") as f:
             json_str = {key: value.to_dict()
                         for key, value in self.__objects.items()}
             json.dump(json_str, f)
@@ -29,7 +30,7 @@ class FileStorage:
         """ reload json string from """
         from models.base_model import BaseModel
         if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r', encoding="utf8") as f:
+            with open(FileStorage.__file_path, 'r', encoding="utf8") as f:
                 try:
                     data = json.load(f)
                     for key, value in data.items():
